bcae2fac8ba5d9308b0da3f8e57289d6e127cab4
AryCuandinata <arycuandinatatjoa@gmail.com>
2021-05-20 12:55:22 +0800

    bottom bar finish

--- app/src/main/java/id/ac/ubaya/informatika/finaltermproject/view/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/finaltermproject/view/MainActivity.kt
@@ -2,11 +2,26 @@ package id.ac.ubaya.informatika.finaltermproject.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import id.ac.ubaya.informatika.finaltermproject.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = Navigation.findNavController(this,R.id.hostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        bottomNav.setupWithNavController(navController)
     }
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,null)
+    }
+
 }
