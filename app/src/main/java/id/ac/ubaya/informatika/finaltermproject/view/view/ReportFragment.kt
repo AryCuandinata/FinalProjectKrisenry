6784493b3c34f013d8deee594ad780ec0ec8d727
AryCuandinata <arycuandinatatjoa@gmail.com>
2021-06-19 16:50:01 +0800

    report, welcome edited

--- app/src/main/java/id/ac/ubaya/informatika/finaltermproject/view/view/ReportFragment.kt
+++ app/src/main/java/id/ac/ubaya/informatika/finaltermproject/view/view/ReportFragment.kt
@@ -1,11 +1,15 @@
 package id.ac.ubaya.informatika.finaltermproject.view.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import id.ac.ubaya.informatika.finaltermproject.R
+import kotlinx.android.synthetic.main.fragment_report.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class ReportFragment : Fragment() {
 
@@ -15,6 +19,15 @@ class ReportFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_report, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val date = LocalDateTime.now()
+        val format = DateTimeFormatter.ofPattern("MMMM yyyy")
+        val currentDate = date.format(format)
+        txtDate.text = currentDate
     }
 
 }
